d198ecbfee6d3bdf459468a316d72f8de9648089
dasturi <oba93@hotmail.com>
2020-02-22 23:17:21 +0100

    answer flip

--- client/src/components/layout/Answers.js
+++ client/src/components/layout/Answers.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Answer from "./Answer";
+import { Flipper, Flipped } from "react-flip-toolkit";
 
 function Answers() {
   const { currentAnswer, currentQuestion, questions } = useSelector(
@@ -8,35 +9,71 @@ function Answers() {
   );
   const question = questions[currentQuestion];
 
+  const shuffleAnswers = () => {
+    const answerDiv = document.querySelector(".answer-div");
+    if (answerDiv) {
+      const children = answerDiv.children;
+      console.log("Children", children.item);
+      while (children.length) {
+        answerDiv.append(
+          children.item(Math.floor(Math.random() * children.length), 1)
+        );
+      }
+    }
+  };
+
+  // if (answerDiv.children()) {
+  //   console.log("answer DIV", answerDiv.children());
+  // }
+
   return (
-    <>
-      <Answer
-        letter="a"
-        answer={decodeURIComponent(question.correct_answer)}
-        selected={currentAnswer === decodeURIComponent(question.correct_answer)}
-      />
-      <Answer
-        letter="b"
-        answer={decodeURIComponent(question.incorrect_answers[0])}
-        selected={
-          currentAnswer === decodeURIComponent(question.incorrect_answers[0])
-        }
-      />
-      <Answer
-        letter="c"
-        answer={decodeURIComponent(question.incorrect_answers[1])}
-        selected={
-          currentAnswer === decodeURIComponent(question.incorrect_answers[1])
-        }
-      />
-      <Answer
-        letter="d"
-        answer={decodeURIComponent(question.incorrect_answers[2])}
-        selected={
-          currentAnswer === decodeURIComponent(question.incorrect_answers[2])
-        }
-      />
-    </>
+    <div className="answer-div">
+      <Flipper flipKey={1}>
+        <Flipped key="a" flipId="a">
+          <Answer
+            key="a"
+            letter="a"
+            answer={decodeURIComponent(question.correct_answer)}
+            selected={
+              currentAnswer === decodeURIComponent(question.correct_answer)
+            }
+          />
+        </Flipped>
+        <Flipped key="b" flipId="b">
+          <Answer
+            key="b"
+            letter="b"
+            answer={decodeURIComponent(question.incorrect_answers[0])}
+            selected={
+              currentAnswer ===
+              decodeURIComponent(question.incorrect_answers[0])
+            }
+          />
+        </Flipped>
+        <Flipped key="c" flipId="c">
+          <Answer
+            key="c"
+            letter="c"
+            answer={decodeURIComponent(question.incorrect_answers[1])}
+            selected={
+              currentAnswer ===
+              decodeURIComponent(question.incorrect_answers[1])
+            }
+          />
+        </Flipped>
+        <Flipped key="d" flipId="d">
+          <Answer
+            key="d"
+            letter="d"
+            answer={decodeURIComponent(question.incorrect_answers[2])}
+            selected={
+              currentAnswer ===
+              decodeURIComponent(question.incorrect_answers[2])
+            }
+          />
+        </Flipped>
+      </Flipper>
+    </div>
   );
 }
 
